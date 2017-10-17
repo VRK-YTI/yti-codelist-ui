69b352550549a14fd0fa6ffc2b8dad13157e9cb3
Miikka Sauramo <miikka.sauramo@phz.fi>
2017-10-17 10:18:22 +0300

    New header and footer

--- src/app/components/navigation-bar.component.ts
+++ src/app/components/navigation-bar.component.ts
@@ -5,8 +5,7 @@ import { Language, LanguageService } from '../services/language.service';
   selector: 'app-navigation-bar',
   styleUrls: ['./navigation-bar.component.scss'],
   template: `
-    <nav class="navbar navbar-toggleable-md navbar bg-inverse">
-      
+    <nav class="navbar">
       <svg viewBox="0 0 50 100" id="beta_suomifi_logo" width="80" height="80">
       <g>
         <path fill="#003479" d="M53,0H2C0.9,0,0,0.9,0,2v51c0,1.1,0.9,2,2,2h51c1.1,0,2-0.9,2-2V2C55,0.9,54.1,0,53,0z"></path>
@@ -19,7 +18,7 @@ import { Language, LanguageService } from '../services/language.service';
       </g>
     </svg>
 
-    <a class="navbar-brand" [routerLink]="['/']"><span>Koodistoeditori</span></a>
+    <a class="navbar-brand" [routerLink]="['/']">Koodistoeditori</a>
     </nav>
   `
 })
