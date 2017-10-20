67659f4b719ea90ac29ca5283544c35c2c522f8d
Miikka Sauramo <miikka.sauramo@phz.fi>
2017-10-20 09:47:31 +0300

    Navigation bar html template separated to own file. Changes to fonts and background color.

--- src/app/components/navigation-bar.component.ts
+++ src/app/components/navigation-bar.component.ts
@@ -4,23 +4,7 @@ import { Language, LanguageService } from '../services/language.service';
 @Component({
   selector: 'app-navigation-bar',
   styleUrls: ['./navigation-bar.component.scss'],
-  template: `
-    <nav class="navbar">
-      <svg viewBox="0 0 50 100" id="beta_suomifi_logo" width="80" height="80">
-      <g>
-        <path fill="#003479" d="M53,0H2C0.9,0,0,0.9,0,2v51c0,1.1,0.9,2,2,2h51c1.1,0,2-0.9,2-2V2C55,0.9,54.1,0,53,0z"></path>
-        <g>
-          <path fill="#FFFFFF" d="M14,20v-5c0-1.1,0.9-2,2-2h5v7"></path>
-          <path fill="#FFFFFF" d="M14,27h7v14c0,0.5-0.4,1-1,1h-5c-0.6,0-1-0.5-1-1"></path>
-          <path fill="#FFFFFF" d="M28,13h13c0.5,0,1,0.4,1,1v6H28"></path>
-          <path fill="#FFFFFF" d="M41,34H28v-7h14v6C42,33.6,41.6,34,41,34z"></path>
-        </g>
-      </g>
-    </svg>
-
-    <h4><a [routerLink]="['/']">Koodistoeditori</a></h4>
-    </nav>
-  `
+  templateUrl: './navigation-bar.component.html',
 })
 export class NavigationBarComponent {
 
