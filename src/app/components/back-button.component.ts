3da9d8e04b9b24255c35a807fd1d4f5613047830
Janne Mäki <janne.maki@evident.fi>
2017-11-16 19:34:25 +0200

    Rename style file name

--- src/app/components/back-button.component.ts
+++ src/app/components/back-button.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-back-button',
-  styleUrls: ['./app-back-button.scss'],
+  styleUrls: ['./back-button.scss'],
   template: `
     <div class="row">
       <div class="col-12">
