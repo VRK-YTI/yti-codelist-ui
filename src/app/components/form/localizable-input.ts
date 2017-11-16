6e9957ac4642b7e3e9f876dc0ef2bc7f9b8f73d2
Janne Mäki <janne.maki@evident.fi>
2017-11-16 11:30:30 +0200

    Localizable inputs also as control value accessors

--- src/app/components/form/localizable-input.ts
+++ src/app/components/form/localizable-input.ts
@@ -1,29 +1,44 @@
-import { Component, Input } from '@angular/core';
-import { Localizable } from '../../entities/localization';
+import { Component, forwardRef, Input } from '@angular/core';
 import { EditableService } from '../../services/editable.service';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { LanguageService } from '../../services/language.service';
+import { Localizable } from '../../entities/localization';
 
 @Component({
   selector: 'app-localizable-input',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => LocalizableInputComponent),
+    multi: true
+  }],
   template: `
     <dl *ngIf="show">
       <dt><label>{{label}}</label></dt>
       <dd>
         <div *ngIf="editing" class="form-group">
-          <input type="text" class="form-control" [(ngModel)]="value[contentLanguage]" />
+          <input type="text" class="form-control" [ngModel]="value[contentLanguage]" (ngModelChange)="onChange($event)" />
         </div>
         <span *ngIf="!editing">{{value | translateValue}}</span>
       </dd>
     </dl>
   `
 })
-export class LocalizableInputComponent {
+export class LocalizableInputComponent implements ControlValueAccessor {
 
   @Input() label: string;
-  @Input() value: Localizable;
+
+  value: Localizable = {};
+
+  private propagateChange: (fn: any) => void = () => {};
+  private propagateTouched: (fn: any) => void = () => {};
 
   constructor(private editableService: EditableService,
               private languageService: LanguageService) {
+  }
+
+  onChange(value: string) {
+    this.value[this.contentLanguage] = value;
+    this.propagateChange(this.value);
   }
 
   get show() {
@@ -36,5 +51,17 @@ export class LocalizableInputComponent {
 
   get contentLanguage() {
     return this.languageService.contentLanguage;
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.propagateTouched = fn;
   }
 }
--- src/app/components/form/localizable-textarea.ts
+++ src/app/components/form/localizable-textarea.ts
@@ -1,29 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
 import { Localizable } from '../../entities/localization';
 import { EditableService } from '../../services/editable.service';
 import { LanguageService } from '../../services/language.service';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-localizable-textarea',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => LocalizableTextareaComponent),
+    multi: true
+  }],
   template: `
     <dl *ngIf="show">
       <dt><label>{{label}}</label></dt>
       <dd>
         <div *ngIf="editing" class="form-group">
-          <textarea rows="3" class="form-control" [(ngModel)]="value[contentLanguage]"></textarea>
+          <textarea rows="3" class="form-control" [ngModel]="value[contentLanguage]" (ngModelChange)="onChange($event)"></textarea>
         </div>
         <span *ngIf="!editing">{{value | translateValue}}</span>
       </dd>
     </dl>
   `
 })
-export class LocalizableTextareaComponent {
+export class LocalizableTextareaComponent implements ControlValueAccessor {
 
   @Input() label: string;
-  @Input() value: Localizable;
+  value: Localizable = {};
+
+  private propagateChange: (fn: any) => void = () => {};
+  private propagateTouched: (fn: any) => void = () => {};
 
   constructor(private editableService: EditableService,
               private languageService: LanguageService) {
+  }
+
+  onChange(value: string) {
+    this.value[this.contentLanguage] = value;
+    this.propagateChange(this.value);
   }
 
   get show() {
@@ -36,5 +50,17 @@ export class LocalizableTextareaComponent {
 
   get contentLanguage() {
     return this.languageService.contentLanguage;
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.propagateTouched = fn;
   }
 }
