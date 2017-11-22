1b964ca8950e2f7d481047897bee44c321f31ffb
Janne Mäki <janne.maki@evident.fi>
2017-11-22 19:41:53 +0200

    Clean up unused logic

--- src/app/components/codescheme/link-show-modal.component.ts
+++ src/app/components/codescheme/link-show-modal.component.ts
@@ -1,9 +1,7 @@
-import { Component, Injectable, Input, OnInit } from '@angular/core';
+import { Component, Injectable, Input } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ExternalReference } from '../../entities/external-reference';
 import { EditableService } from '../../services/editable.service';
-import { PropertyType } from '../../entities/property-type';
-import { DataService } from '../../services/data.service';
 
 @Injectable()
 export class LinkShowModalService {
@@ -23,36 +21,11 @@ export class LinkShowModalService {
   templateUrl: './link-show-modal.component.html',
   providers: [EditableService]
 })
-export class LinkShowModalComponent implements OnInit {
+export class LinkShowModalComponent {
 
   @Input() link: ExternalReference;
 
-  initialized: boolean;
-  propertyTypes: PropertyType[];
-  propertyType: PropertyType;
-
-  constructor(private dataService: DataService,
-              private modal: NgbActiveModal) {
-  }
-
-  ngOnInit() {
-    this.dataService.getPropertyTypes('ExternalReference').subscribe(types => {
-      this.propertyTypes = types;
-      if (this.link.propertyType !== undefined) {
-        this.propertyType = this.link.propertyType;
-      } else if (this.propertyTypes !== undefined && this.propertyTypes.length > 0) {
-        this.propertyType = this.propertyTypes[0];
-        this.link.propertyType = this.propertyType;
-      } else {
-        console.log('Issue with PropertyType not being set!');
-      }
-
-      this.initialized = true;
-    });
-  }
-
-  get initializing(): boolean {
-    return this.propertyTypes == null && this.initialized;
+  constructor(private modal: NgbActiveModal) {
   }
 
   close() {
