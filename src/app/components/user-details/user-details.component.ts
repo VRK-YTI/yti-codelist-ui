beffbfca2f34b6b4e21571ea69df29372dc29120
JereVeijalainen <jereveijalainen82@gmail.com>
2017-12-19 18:08:32 +0200

    Show user´s organizations and roles in user details

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -2,9 +2,19 @@ import { Component, OnDestroy } from '@angular/core';
 import { Role, UserService } from 'yti-common-ui/services/user.service';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { index } from 'yti-common-ui/utils/array';
+import { comparingLocalizable } from 'yti-common-ui/utils/comparator';
 import { TranslateService } from 'ng2-translate';
 import { LanguageService } from '../../services/language.service';
 import { LocationService } from '../../services/location.service';
+import { DataService } from '../../services/data.service';
+import { Organization } from '../../entities/organization';
+
+interface UserOrganizationRoles {
+  organization?: Organization;
+  roles: Role[];
+  requests: Role[];
+}
 
 @Component({
   selector: 'app-user-details',
@@ -15,9 +25,15 @@ export class UserDetailsComponent implements OnDestroy  {
 
   private loggedInSubscription: Subscription;
 
+  allOrganizations: Organization[];
+  allOrganizationsById: Map<string, Organization>;
+  selectedOrganization: Organization|null = null;
+  requestsInOrganizations = new Map<string, Set<Role>>();
+
   constructor(private router: Router,
               private userService: UserService,
               private locationService: LocationService,
+              private dataService: DataService,
               private languageService: LanguageService,
               private translateService: TranslateService) {
 
@@ -28,6 +44,11 @@ export class UserDetailsComponent implements OnDestroy  {
     });
 
     locationService.atUserDetails();
+
+    this.dataService.getOrganizations().subscribe(organizations => {
+      this.allOrganizations = organizations;
+      this.allOrganizationsById = index(organizations, org => org.id);
+    });
   }
 
   ngOnDestroy() {
@@ -39,7 +60,26 @@ export class UserDetailsComponent implements OnDestroy  {
   }
 
   get loading() {
-    return false;
+    return !this.allOrganizations || !this.requestsInOrganizations;
   }
 
+  get userOrganizations(): UserOrganizationRoles[] {
+
+    const organizationIds = new Set<string>([
+      ...Array.from(this.user.rolesInOrganizations.keys()),
+      ...Array.from(this.requestsInOrganizations.keys())
+    ]);
+
+    const result = Array.from(organizationIds.values()).map(organizationId => {
+      return {
+        organization: this.allOrganizationsById.get(organizationId),
+        roles: Array.from(this.user.getRoles(organizationId)),
+        requests: Array.from(this.requestsInOrganizations.get(organizationId) || [])
+      }
+    });
+
+    result.sort(comparingLocalizable<UserOrganizationRoles>(this.languageService, org => org.organization ? org.organization.prefLabel : {}));
+
+    return result;
+  }
 }
