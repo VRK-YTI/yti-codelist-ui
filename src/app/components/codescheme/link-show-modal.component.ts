6f312570eb7433d2f8dd73c0f7670c24feaf3b37
JereVeijalainen <jereveijalainen82@gmail.com>
2018-01-30 09:40:38 +0200

    YTI-471 Customize link show modal for general license links
    Show icons with links for Creative Commons Attribution (CC BY 4.0) and Creative Commons Zero (CC0) licenses.

--- src/app/components/codescheme/link-show-modal.component.ts
+++ src/app/components/codescheme/link-show-modal.component.ts
@@ -1,7 +1,17 @@
 import { Component, Injectable, Input } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ExternalReference } from '../../entities/external-reference';
+import { ExternalReference, CCBy40LicenseLinkId, CC0LicenseLinkId } from '../../entities/external-reference';
 import { EditableService } from '../../services/editable.service';
+import { Localizable } from 'yti-common-ui/types/localization';
+import { LanguageService } from '../../services/language.service';
+
+const CCBy40LicenseImage = 'assets/images/ccby40-icon-88x31.png';
+const CC0LicenseImage = 'assets/images/cc0-icon-88x31.png';
+const titleCCBy40: Localizable = {
+  'fi': 'Creative Commons -lisenssi',
+  'sv': 'Creative Commons-licens',
+  'en': 'Creative Commons License'
+};
 
 @Injectable()
 export class LinkShowModalService {
@@ -19,16 +29,33 @@ export class LinkShowModalService {
 @Component({
   selector: 'app-link-show-modal',
   templateUrl: './link-show-modal.component.html',
+  styleUrls: ['./link-show-modal.component.scss'],
   providers: [EditableService]
 })
 export class LinkShowModalComponent {
 
   @Input() externalReference: ExternalReference;
+  
+  srcCCBy40 = CCBy40LicenseImage;
+  srcCC0 = CC0LicenseImage;
 
-  constructor(private modal: NgbActiveModal) {
+  constructor(private modal: NgbActiveModal,
+              private languageService: LanguageService) {
   }
 
   close() {
     this.modal.dismiss('cancel');
   }
+
+  get CCBy40LicenseLink() {
+    return this.externalReference.id === CCBy40LicenseLinkId;
+  }
+
+  get CC0LicenseLink() {
+    return this.externalReference.id === CC0LicenseLinkId;
+  }
+
+  get titleCCBy40() {
+    return this.languageService.translate(titleCCBy40);
+  }  
 }
--- src/app/entities/external-reference.ts
+++ src/app/entities/external-reference.ts
@@ -1,6 +1,9 @@
 import { Localizable } from 'yti-common-ui/types/localization';
 import { PropertyType } from './property-type';
 
+export const CCBy40LicenseLinkId = '9a25f7fc-e4be-11e7-82ab-479f4f288376';
+export const CC0LicenseLinkId = '9553aad0-e4be-11e7-81e9-1faf2d228a02';
+
 export class ExternalReference {
 
   id = '';
