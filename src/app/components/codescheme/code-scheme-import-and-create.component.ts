6de69e5ba3119f0beb8586989afb0af0dd0cb668
lammitomi <tomi.lammi@gatewaytechnolabs.fi>
2018-02-12 15:35:05 +0200

    Revert the changes on wrong place

--- src/app/components/codescheme/code-scheme-import-and-create.component.ts
+++ src/app/components/codescheme/code-scheme-import-and-create.component.ts
@@ -1,27 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CodeSchemeImportModalService } from './code-scheme-import-modal.component';
-import {DataService} from '../../services/data.service';
-import {CodeRegistry} from '../../entities/code-registry';
 
 @Component({
   selector: 'app-code-scheme-import-and-create',
   templateUrl: './code-scheme-import-and-create.component.html',
   styleUrls: ['./code-scheme-import-and-create.component.scss']
 })
-export class CodeSchemeImportAndCreateComponent implements OnInit {
-
-  codeRegistries: CodeRegistry[];
+export class CodeSchemeImportAndCreateComponent {
 
   constructor(private router: Router,
-              private dataService: DataService,
               private codeSchemeImportModalService: CodeSchemeImportModalService) {
-  }
-
-  ngOnInit() {
-    this.dataService.getCodeRegistriesForUser().subscribe(codeRegistries => {
-      this.codeRegistries = codeRegistries;
-    });
   }
 
   back() {
