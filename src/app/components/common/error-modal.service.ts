0fd22897c87d5775df70ff201be0bd87aab1d196
Janne Mäki <janne.maki@evident.fi>
2018-03-06 17:46:54 +0200

    Take body out of response

--- src/app/components/common/error-modal.service.ts
+++ src/app/components/common/error-modal.service.ts
@@ -1,6 +1,6 @@
-import { ApiResponseType } from '../../services/api-schema';
 import { ErrorModalService } from 'yti-common-ui/components/error-modal.component';
 import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
 
 @Injectable()
 export class CodeListErrorModalService {
@@ -8,15 +8,22 @@ export class CodeListErrorModalService {
   constructor(private errorModalService: ErrorModalService) {
   }
 
-  openSubmitError(error: ApiResponseType) {
+  openSubmitError(error: any) {
 
     const showDebug = false; // TODO luetaan oikeasti jostain ympäristökonfiguraatiosta
 
-    this.errorModalService.openWithOptions({
-      title: 'Submit error',
-      body: error.meta.message,
-      bodyParams: error.meta.entityIdentifier,
-      err: showDebug ? error : undefined
-    });
+    if (error instanceof Response) {
+
+      const body = error.json();
+
+      this.errorModalService.openWithOptions({
+        title: 'Submit error',
+        body: body.meta.message,
+        bodyParams: body.meta.entityIdentifier,
+        err: showDebug ? error : undefined
+      });
+    } else {
+      this.errorModalService.openSubmitError(showDebug ? error : undefined);
+    }
   }
 }
