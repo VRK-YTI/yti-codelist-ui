9eef2d2a579d66e89f8abb8a245a83238daa41f9
Antti Tohmo <antti.tohmo@gofore.com>
2018-08-22 11:48:13 +0300

    Removing extra commented line.

--- src/app/components/navigation/navigation-bar.component.ts
+++ src/app/components/navigation/navigation-bar.component.ts
@@ -65,7 +65,6 @@ export class NavigationBarComponent {
   set language(language: Language) {
     this.languageService.language = language;
     if (this.languageService.language !== language) {
-      // window.location.reload();
       this.router.navigate([this.route.root]);
     }
   }
