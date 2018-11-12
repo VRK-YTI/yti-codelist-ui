a7c429d71d8851bf4637c0dc436963f8004f788e
Antti Tohmo <antti.tohmo@gofore.com>
2018-11-12 12:51:27 +0200

    YTI-1222: Language service added support for local storage regarding content and main language.

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Language, Localizable, Localizer } from 'yti-common-ui/types/localization';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { getFromLocalStorage, setToLocalStorage } from 'yti-common-ui/utils/storage';
 
 export { Language };
 
 @Injectable()
 export class LanguageService implements Localizer {
 
-  language$ = new BehaviorSubject<Language>('fi');
-  contentLanguage$ = new BehaviorSubject<Language>('fi');
+  private static readonly LANGUAGE_KEY: string = 'yti-codelist-ui.language-service.language';
+  private static readonly CONTENT_LANGUAGE_KEY: string = 'yti-codelist-ui.language-service.content-language';
+
+  language$ = new BehaviorSubject<Language>(getFromLocalStorage(LanguageService.LANGUAGE_KEY, 'fi'));
+  contentLanguage$ = new BehaviorSubject<Language>(getFromLocalStorage(LanguageService.CONTENT_LANGUAGE_KEY, 'fi'));
+  translateLanguage$ = new BehaviorSubject<Language>(this.language);
 
   constructor(private translateService: TranslateService) {
 
@@ -18,9 +23,8 @@ export class LanguageService implements Localizer {
     translateService.setDefaultLang('en');
     this.language$.subscribe(lang => this.translateService.use(lang));
 
-    this.language$.subscribe(language => {
-      this.contentLanguage = language;
-    });
+    combineLatest(this.language$, this.contentLanguage$)
+      .subscribe(([lang, contentLang]) => this.translateLanguage$.next(contentLang || lang));
   }
 
   get language(): Language {
@@ -30,6 +34,7 @@ export class LanguageService implements Localizer {
   set language(language: Language) {
     if (this.language !== language) {
       this.language$.next(language);
+      setToLocalStorage(LanguageService.LANGUAGE_KEY, language);
     }
   }
 
@@ -40,11 +45,8 @@ export class LanguageService implements Localizer {
   set contentLanguage(language: Language) {
     if (this.contentLanguage !== language) {
       this.contentLanguage$.next(language);
+      setToLocalStorage(LanguageService.CONTENT_LANGUAGE_KEY, language);
     }
-  }
-
-  get translateLanguage$(): Observable<Language> {
-    return this.contentLanguage$;
   }
 
   translate(localizable: Localizable, useUILanguage = false): string {
