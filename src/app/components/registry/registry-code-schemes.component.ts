4e74bf5d9f9565df6c0f78c6f4aa2af6bd305e48
Antti Tohmo <antti.tohmo@gofore.com>
2019-09-13 14:24:53 +0300

    Bugfixing registry codescheme listing UI styles and registry access from uri link to fetch data including loading animation support.

--- src/app/components/registry/registry-code-schemes.component.ts
+++ src/app/components/registry/registry-code-schemes.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CodeRegistry } from '../../entities/code-registry';
 import { DataService } from '../../services/data.service';
 import { CodeScheme } from '../../entities/code-scheme';
@@ -10,17 +10,32 @@ import { LanguageService } from '../../services/language.service';
   templateUrl: './registry-code-schemes.component.html',
   styleUrls: ['./registry-code-schemes.component.scss']
 })
-export class RegistryCodeSchemesComponent {
+export class RegistryCodeSchemesComponent implements OnInit {
 
   @Input() codeRegistry: CodeRegistry;
   @Input() codeSchemes: CodeScheme[];
 
+  loading: boolean;
+
   constructor(private dataService: DataService,
+              private route: ActivatedRoute,
               private router: Router,
               public languageService: LanguageService) {
   }
 
+  ngOnInit() {
+
+    if (!this.codeSchemes || this.codeSchemes.length === 0) {
+      const registryCodeValue = this.route.snapshot.params.registryCode;
+      this.dataService.getCodeSchemesForCodeRegistry(registryCodeValue).subscribe(codeSchemes => {
+        this.codeSchemes = codeSchemes;
+        this.loading = false;
+      });
+    }
+  }
+
   viewCodeScheme(codeScheme: CodeScheme) {
+
     this.router.navigate(codeScheme.route);
   }
 }
